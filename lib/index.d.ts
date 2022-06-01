608973a218fcd0724b12374aeab06c54bd1674d8
Moses <mohemos@gmail.com>
2022-06-01 23:36:23 +0100

    first commit

---
+++ lib/index.d.ts
@@ -0,0 +1,2 @@
+export declare function globalErrorHandler(err: Error): void;
+//# sourceMappingURL=index.d.ts.map
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export function globalErrorHandler(err: Error): void {
+  console.log("=======Unhandled error=======/n/n", err);
+}
