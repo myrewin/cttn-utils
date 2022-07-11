e346a918e754ddbd281ea58fa9afc69fd1933a35
Ndamati Precious <ndamatiprecious@gmail.com>
2022-07-11 13:09:53 +0100

    added

--- src/index.ts
+++ src/index.ts
@@ -23,4 +23,16 @@ export function parseJSON(value: any): any {
   } catch (err) {
     return value;
   }
-}
+
+
+// export const connectSql = ({db_host,db_name,db_port,db_user}:{db_host: string, db_name:string, db_port: number, db_user:string})=>{
+//   const db = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USERNAME,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: "mysql",
+//   }
+// );
+// }
