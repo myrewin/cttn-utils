baca9bb2a049e2ae1c5b93455c48db00cb1aebcf
Moses Peter <mohemos@gmail.com>
2022-08-24 09:27:52 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * from "./utils/index.js";
-export * from "./database/redis.js";
-export * from "./database/sqlDb.js";
 export * from "./errors/index.js";
 export * from "./kafka/index.js";
+export * from "./database/redis.js";
+export * from "./database/sqlDb.js";
